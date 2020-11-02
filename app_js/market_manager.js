a22047e05eeca4b0cc4ab09c3456e34b1755ea50
adarelk2 <63156864+adarelk2@users.noreply.github.com>
2020-11-02 22:20:38 +0200

    Update market_manager.js

--- app_js/market_manager.js
+++ app_js/market_manager.js
@@ -1,5 +1,5 @@
 import {data,resetData} from "./user_manager.js";
-import ProductClass from "./ProductClass.js";
+import Product from "./Product.js";
 import UserClass from "./user_class.js";
 export let dataApi = [];
 export const addToCart = (_obj)=>
@@ -51,7 +51,7 @@ document.getElementById("id_items").innerHTML = "";
 if(_ar.level ==true)
 {
    _ar.items.map(items=>{
-      let product = new ProductClass("#id_items",items.id,items.name,items.subject,items.price,items.cate);
+      let product = new Product("#id_items",items.id,items.name,items.subject,items.price,items.cate);
       product.adminRender();
     })
 }
@@ -59,7 +59,7 @@ else
 {
 
    _ar.items.map(items=>{
-      let product = new ProductClass("#id_items",items.id,items.name,items.subject,items.price,items.cate,items.img);
+      let product = new Product("#id_items",items.id,items.name,items.subject,items.price,items.cate,items.img);
       product.render();
     })
        }
@@ -75,7 +75,7 @@ if(itemsdiv)
 checkButtonOrder();
 document.getElementById("id_cartItems").innerHTML = "";
 _obj.map(items=>{
-let product = new ProductClass("#id_cartItems",items.id,items.name,items.subject,items.price);
+let product = new Product("#id_cartItems",items.id,items.name,items.subject,items.price);
 product.getCart(items.index);
 })
 }
